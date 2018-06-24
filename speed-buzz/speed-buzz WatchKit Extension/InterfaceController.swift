1448b71bbb86193b9803efe2a03634a66d96b1e5
benjamindimant <benjamin@dimant.com>
2018-06-24 10:21:59 +0100

    Add skeleton files

---
+++ speed-buzz/speed-buzz WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  speed-buzz WatchKit Extension
+//
+//  Created by Benjamin Dimant on 6/24/18.
+//  Copyright © 2018 Company Ltd. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ speed-buzz/speed-buzz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  speed-buzz
+//
+//  Created by Benjamin Dimant on 6/24/18.
+//  Copyright © 2018 Company Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
